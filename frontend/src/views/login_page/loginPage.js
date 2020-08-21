50fb35fee91e4844f1b7c682eb72c029cabe9a3c
OLADIPO-K <akinspreciousola@gmail.com>
2020-08-21 23:59:37 +0100

    created frontend client-side skeleton

---
+++ frontend/src/views/login_page/loginPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class LoginPage extends React.Component {
+    render(){
+        return (
+            <div>
+                {/* HTML CODE FOR LOGIN PAGE */}
+                <h1>LOGIN PAGE</h1>
+            </div>
+        )
+    }
+}
+
+export default LoginPage;
---
+++ frontend/src/views/registration_page/registrationPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class RegistrationPage extends React.Component {
+    render(){
+        return (
+            <div>
+                {/* HTML CODE FOR REGISTRATION PAGE */}
+                <h1>REGISTRATION PAGE</h1>
+            </div>
+        )
+    }
+}
+
+export default RegistrationPage;
