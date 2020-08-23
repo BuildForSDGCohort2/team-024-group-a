faea9655f1f7a5f636172715b459a8b27f789c77
Anani oluwatobiloba <ananioluwatobiloba2000@gmail.com>
2020-08-24 00:37:08 +0100

    removed nav in signup page

--- frontend/src/views/registration_page/registrationPage.js
+++ frontend/src/views/registration_page/registrationPage.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import Nav from '../../components/nav/nav';
+import {Link} from 'react-router-dom';
 
 class RegistrationPage extends React.Component {
     render(){
         return (
             <div>
-                <Nav />
                 {/* HTML CODE FOR REGISTRATION PAGE */}
                 <h1>REGISTRATION PAGE</h1>
+                <Link to="/">Go home</Link>
             </div>
         )
     }
