bedb01e4e73040ae88ceb6ee1e78b0f97f321546
samuelhenshaw2020 <samuelhenshaw2020@gmail.com>
2020-08-24 09:56:06 +0100

    fixed bugs and implemented best practice

--- backend/src/controllers/guest.controller.js
+++ backend/src/controllers/guest.controller.js
@@ -6,19 +6,19 @@ class GuestController {
 
     }
 
-    welcome =  async (req, res)=>{
+    welcome =  async (req, res) =>{
         let message = await `Welcome to ${_app.APP_NAME} Platform`;
         return res.status(200).format({
-            html: function(){
+            html: () =>{
                 res.send(message);
             },
-            json: function(){
+            json: () =>{
                 res.status(200).json({message});
             },
-            text: function(){
+            text: () =>{
                 res.status(200).end(message);
             },
-            default: function(){
+            default: () =>{
                 res.status(200).json({message});
             }
         });
--- backend/src/routes/hospitals.route.js
+++ backend/src/routes/hospitals.route.js
@@ -1,8 +1,8 @@
 const router = require("express").Router();
 
 
-router.get("/",  [], function(req, res){
-    return res.status(200).json({message: "Hospitals routes"})
+router.get("/", [], function(req, res){
+    return res.status(200).json({message: "Hospitals routes"});
 });
 
 
