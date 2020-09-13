0dc1602d068062551545d99411877dca9eddd6c5
John Adeniran <jasolajohn@gmail.com>
2020-09-13 22:02:35 +0100

    db connect

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,6 @@ const os = require("os");
 require("dotenv").config({path: __dirname + "/.env"});
 
 
-//will be removed from here but for now let it be
-process.env.SECRET_KEY = "df431efd1ec7484da96a3c327f86881559a7cf55bbd4b6744054abcec76ksbdki";
 
 if(cluster.isMaster){
 
