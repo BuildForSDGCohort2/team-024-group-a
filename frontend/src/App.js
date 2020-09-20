c001ad4a89be871f3daf56d5feba75cc68401d91
OLADIPO-K <akinspreciousola@gmail.com>
2020-09-20 23:01:56 +0100

    feat: created the doctor's dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,29 +3,31 @@ import "./App.css";
 import "./App.less";
 import {Switch, Route, BrowserRouter as Router} from "react-router-dom";
 
-import LandingPage from "./views/landing_page/landingPage";
+//import LandingPage from "./views/landing_page/landingPage";
 import LoginPage from "./views/login_page/loginPage";
 import RegistrationPage from "./views/registration_page/registrationPage";
 import NotFound from "./views/notfound_page/notfound";
 import Footer from "./components/footer/footer";
 
+import DoctorDashboard from "./dashboards/doctor_dashboard/doctorDashboard";
+
 class App extends React.Component {
 
   render(){
-      return (
-        <div className="App">
-           <Router>
-              <Switch>
-                <Route  exact={true} path="/" component={LandingPage}/>
-                <Route  path="/signup" component={RegistrationPage}/>
-                <Route  path="/login" component={LoginPage}/>
-                <Route component={NotFound} />
-              </Switch>
-         </Router>
+    return (
+      <div className="App">
+        <Router>
+          <Switch>
+            <Route  exact={true} path="/" component={DoctorDashboard}/>
+            <Route  path="/signup" component={RegistrationPage}/>
+            <Route  path="/login" component={LoginPage}/>
+            <Route component={NotFound} />
+          </Switch>
+        </Router>
 
-         <Footer />
-        </div>
-      );
+        <Footer />
+      </div>
+   );
   };
 
 };
